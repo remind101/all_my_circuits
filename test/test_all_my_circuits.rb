1e895c14e8ba4489f1813b80922e6475bd708e37
Vlad Yarotsky <vlad@remind101.com>
2015-05-21 12:56:01 -0700

    A slightly cleaner approach, same idea

--- test/test_all_my_circuits.rb
+++ test/test_all_my_circuits.rb
@@ -1,5 +1,5 @@
-require 'minitest_helper'
-require 'all_my_circuits'
+require "minitest_helper"
+require "all_my_circuits"
 
 class TestAllMyCircuits < AllMyCircuitsTC
   test "has version number" do
@@ -11,7 +11,7 @@ class TestAllMyCircuits < AllMyCircuitsTC
       @seconds = 1
     end
 
-    def now
+    def timestamp
       @seconds
     end
 
@@ -23,85 +23,73 @@ class TestAllMyCircuits < AllMyCircuitsTC
   def setup
     super
     @fake_clock = FakeClock.new
-  end
-
-  test "does something useful" do
-    breaker = AllMyCircuits::Breaker.new(
+    @breaker = AllMyCircuits::Breaker.new(
       name: "test service circuit breaker",
       strategy: {
         name: :percentage_over_window,
         measure_window_seconds: 4,
-        threshold_percent: 50
-      },
-      sleep_window_seconds: 4,
-      clock: @fake_clock
+        volume_threshold: 1,
+        failure_rate_percent_threshold: 50,
+        sleep_seconds: 4,
+        clock: @fake_clock
+      }
     )
+  end
 
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+  test "trips the breaker and recovers upon first successful request" do
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance
-    assert_equal :failed, run_through_breaker(breaker) { raise "massive fail" }
+    assert_equal :failed, run_through_breaker { raise "massive fail" }
     @fake_clock.advance
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance
-    assert_equal :failed, run_through_breaker(breaker) { raise "another massive fail that trips the breaker" }
+    assert_equal :failed, run_through_breaker { raise "another massive fail that trips the breaker" }
     @fake_clock.advance
 
     4.times do
-      assert_equal :skipped, run_through_breaker(breaker) { fail "this does not happen" }
+      assert_equal :skipped, run_through_breaker { refute true, "this does not happen" }
       @fake_clock.advance
     end
 
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance
-    assert_equal :failed, run_through_breaker(breaker) { raise "failure that does not trip the breaker" }
+    assert_equal :failed, run_through_breaker { raise "failure that does not trip the breaker" }
     @fake_clock.advance
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance(10)
   end
 
   test "trips the breaker again if first call after reenable_after interval has failed" do
-    breaker = AllMyCircuits::Breaker.new(
-      name: "test service circuit breaker",
-      strategy: {
-        name: :percentage_over_window,
-        measure_window_seconds: 4,
-        threshold_percent: 50
-      },
-      sleep_window_seconds: 4,
-      clock: @fake_clock
-    )
-
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance
-    assert_equal :failed, run_through_breaker(breaker) { raise "massive fail" }
+    assert_equal :failed, run_through_breaker { raise "massive fail" }
     @fake_clock.advance
-    assert_equal :succeeded, run_through_breaker(breaker) { :success }
+    assert_equal :succeeded, run_through_breaker { :success }
     @fake_clock.advance
-    assert_equal :failed, run_through_breaker(breaker) { raise "another massive fail that trips the breaker" }
+    assert_equal :failed, run_through_breaker { raise "another massive fail that trips the breaker" }
     @fake_clock.advance
 
     4.times do
-      assert_equal :skipped, run_through_breaker(breaker) { refute "this does not happen" }
+      assert_equal :skipped, run_through_breaker { refute "this does not happen" }
       @fake_clock.advance
     end
 
-    assert_equal :failed, run_through_breaker(breaker) { raise "trips the breaker again" }
+    assert_equal :failed, run_through_breaker { raise "trips the breaker again" }
     @fake_clock.advance
-    assert_equal :skipped, run_through_breaker(breaker) { fail "this does not happen" }
+    assert_equal :skipped, run_through_breaker { fail "this does not happen" }
   end
 
-  def run_through_breaker(breaker)
+  def run_through_breaker
     begin
-      breaker.run do
+      @breaker.run do
         yield
       end
       :succeeded
     rescue AllMyCircuits::BreakerOpen
       :skipped
     rescue
+      # p $!, $!.backtrace.first(2).join(",")
       :failed
     end
   end
-
-  # make it global within process
 end
